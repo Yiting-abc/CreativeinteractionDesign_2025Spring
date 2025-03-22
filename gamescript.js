30a2148cd692b6e4974b433b1df8464972093a92
Yiting-abc <2584591582@qq.com>
2025-03-22 15:52:29 +0800

    game

--- gamescript.js
+++ gamescript.js
@@ -73,21 +73,32 @@ function sayHello(){
   window.alert("你可以通过键盘上的箭头移动颜色较深的方块，但前提是要先点击方块所对应的字母:b！蓝色;o！橘色;v！紫色;p！粉色;g！绿色,将方块移动到对应位置后点击右下角的完成按钮")
 }
 
+document.querySelectorAll('#box')
+
 const targetPositions = {
  box1: { x: 310, y: 150 },
  box2: { x: 370, y: 150 },
-};
+ box3: { x: 490, y: 150 },
+ box4: { x: 250, y: 150 },
+ box5: { x: 430, y: 150 },
 
+};
 document.getElementById('fanishButton').addEventListener('click', () => {
- const allInPosition = Array.from(document.querySelectorAll('.box1,.box2')).every(box => {
+ const allInPosition = Array.from(document.querySelectorAll('#box1,#box2,#box3,#box4,#box5')).every(box => {
+   const TOLERANCE = 5
    const currentX = box.offsetLeft;
    const currentY = box.offsetTop;
    const target = targetPositions[box.id];
-   return currentX === target.x && currentY === target.y; // 添加返回值
+   return (
+      Math.abs(box.offsetLeft - target.x) <= TOLERANCE &&
+      Math.abs(box.offsetTop - target.y) <= TOLERANCE
+    );
+   return currentX === target.x && currentY === target.y;
  });
 
 
    if (allInPosition) {
+       document.getElementById("game").style.display = "none";
        document.getElementById('successMessage').style.display = 'block';
        activeBox = null;
      } else {
